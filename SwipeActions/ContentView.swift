e140a4f727a52b2118b132acf91c0f9d8c6b06a8
Juan Camilo Marín Ochoa <99774186+jcamilo1599@users.noreply.github.com>
2022-09-22 13:03:57 -0500

    Update ContentView.swift

--- SwipeActions/ContentView.swift
+++ SwipeActions/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    ForEach(1..<5) { i in
+                        Text("\(i)")
+                            .swipeActions(edge: .leading) {
+                                Button {
+                                    counter += i
+                                } label: {
+                                    Label("Sumar \(i)", systemImage: "plus.circle")
+                                }
+                                .tint(.blue)
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button {
+                                    counter -= i
+                                } label: {
+                                    Label("Restar \(i)", systemImage: "minus.circle")
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+                
+                Section {
+                    ForEach(1..<5) { i in
+                        Text("\(i)")
+                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                Button("Sumar") {
+                                    counter += i
+                                }
+                                .tint(.blue)
+                                
+                                Button("Restar") {
+                                    counter -= i
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Contador: \(counter)")
         }
-        .padding()
     }
 }
 
