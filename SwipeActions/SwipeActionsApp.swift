5db3630640c13884e9ba5513ac5c8d45c692444e
Juan Camilo Marín Ochoa <99774186+jcamilo1599@users.noreply.github.com>
2022-09-22 12:53:07 -0500

    Initial Commit

---
+++ SwipeActions/SwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionsApp.swift
+//  SwipeActions
+//
+//  Created by Juan Camilo Marín Ochoa on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
